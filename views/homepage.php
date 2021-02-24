5f04e4206158a2afec0ad4dab496f1f391e396ec
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-02-24 11:12:13 +0100

    added role for user and admin

--- views/homepage.php
+++ views/homepage.php
@@ -13,6 +13,9 @@ session_start();
 
 if(isset($_SESSION['sess_user_id']) && $_SESSION['sess_user_id'] != "") {
   echo '<h1>Welcome '.$_SESSION['sess_user_name'].'</h1>';
+  if(isset($_SESSION['sess_role']) && $_SESSION['sess_role'] == "admin") {
+    echo "Du är inloggad som <b>Admin</b><br />";
+  }
   echo '<h4><a href="../includes/handleLogout.php">Logout</a></h4>';
 } else { 
   echo "Vänligen logga in igen <a href='login.php'>login</a>";
