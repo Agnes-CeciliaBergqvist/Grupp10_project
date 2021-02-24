22ad571562bbf77489b26ad392ddc24de051999b
DavidSaupe <david.saupe@gmail.com>
2021-02-24 11:42:35 +0100

    view posts

--- views/viewPosts.php
+++ views/viewPosts.php
@@ -10,10 +10,15 @@
 
 <?php 
 session_start();
-
+include("../includes/database_connection.php");
 echo "You are logged in as " . $_SESSION['sess_user_name']. "!";
 
-
+$stm = $db->query("SELECT id, username, title, message, category  FROM posts");
+echo "<pre>";
+while ($row = $stm->fetch()) {
+    echo $row['id'] . " " . $row['username'] . " " . $row['title'] . " " . $row['message'] . " " . $row['category'] . "<br />";
+}
+echo "</pre>";
 ?>
     
 </body>
