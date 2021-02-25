fb79c1ce258901846818a2da3a9f42d6dac12f25
DavidSaupe <david.saupe@gmail.com>
2021-02-25 13:06:58 +0100

    completering till Agnes-edit

--- views/edit.php
+++ views/edit.php
@@ -16,23 +16,28 @@ if (isset($_GET['id'])) {
     $stm = $db->query("SELECT * FROM posts WHERE id=$post_id");
     
     while($row = $stm->fetch())  {
-        $username = $row['username'];
+        $message = $row['message'];
+        $title = $row['title'];
+        $category = $row['category'];
     }
-    // ?>
 
-<form method="POST" action ="../includes/handleEdit.php?">
-    <input type="hidden" name="id" value="<?php echo $post_id; ?>"/>
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" value="<?php echo $username; ?>"/><br />
+    if(isset($_SESSION['sess_role']) && $_SESSION['sess_role'] == "admin") {
+     ?>  
+        <form method="POST" action ="../includes/handleEdit.php?">
+        <input type="hidden" name="id" value="<?php echo $id; ?>"/><br />
         <label for="title">Title:</label>
-        <input type="text" name="title" id="title"/><br />
+        <input type="text" name="title" id="title" value="<?php echo $title; ?>"/><br />
         <label for="newMessage">Message:</label>
-        <input type="textarea" name="newMessage" id="newMessage" value="<?php echo $newMessage; ?>/><br />
+        <input type="textarea" name="newMessage" id="newMessage" value="<?php echo $message; ?>"/><br />
         <label for="category">Category:</label>
-        <input type="text" name="category" id="category"/><br />
-    <input type="submit" name="edit_user" value="edit">
-
-</form>
-<?php
+        <input type="text" name="category" id="category" value="<?php echo $category; ?>" /><br />
+        <input type="submit" name="edit_user" value="edit"> 
+        </form>
+    <?php
+    }
+    else {
+        echo "You are not an admin so you can not edit posts!";
+    }
+    
 }
  ?>
--- views/viewPosts.php
+++ views/viewPosts.php
@@ -17,8 +17,11 @@ $stm = $db->query("SELECT id, username, title, message, category  FROM posts");
 echo "<pre>";
 while ($row = $stm->fetch()) {
     $blogg_id = $row['id'];
+
+    if(isset($_SESSION['sess_role']) && $_SESSION['sess_role'] == "admin"){
     echo "<a href='../includes/handleDelete.php?delete_id=$blogg_id'>Delete</a>";
     echo "<a href='../views/edit.php?id=$blogg_id'>Edit</a>";
+    }
 ?>
 
     <?php echo $row['id'] . " " . $row['username'] . " " . $row['title'] . " " . $row['message'] . " " . $row['category'] . "<br />" ;
