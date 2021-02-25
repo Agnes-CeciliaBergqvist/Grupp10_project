394367d491b2b6613abd5acf8709e3bcd48befdc
DavidSaupe <david.saupe@gmail.com>
2021-02-25 13:44:40 +0100

    början på comments

--- views/viewPosts.php
+++ views/viewPosts.php
@@ -22,10 +22,18 @@ while ($row = $stm->fetch()) {
     echo "<a href='../includes/handleDelete.php?delete_id=$blogg_id'>Delete</a>";
     echo "<a href='../views/edit.php?id=$blogg_id'>Edit</a>";
     }
-?>
-
-    <?php echo $row['id'] . " " . $row['username'] . " " . $row['title'] . " " . $row['message'] . " " . $row['category'] . "<br />" ;
+echo $row['id'] . " " . $row['username'] . " " . $row['title'] . " " . $row['message'] . " " . $row['category'] . "<br />" ;
 echo "</pre>";
+
+// echo "<form method='POST' action='viewPosts.php'>
+//       <input type='submit' name='commentBtn' value='comment!'>
+//       </form>";
+
+// if(isset($_POST['commentBtn'])) {
+//     echo "<form method='POST' action='viewPosts.php'>
+//           <input type='textarea' name='comment'>
+//           </form>"; 
+// }
 }
 ?>
 
