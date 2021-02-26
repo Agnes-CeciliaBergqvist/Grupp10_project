378e5eb4425e12816eb07debe46bbea2c2ed19aa
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-02-26 16:28:43 +0100

    fixed so img is visiable

--- views/viewPosts.php
+++ views/viewPosts.php
@@ -30,7 +30,7 @@ while ($row = $stm->fetch()) {
 
 
     }
-    echo $row['id'] . " " . $row['username'] . " " . $row['title'] . '<img src="' . $row['image'] . '"/> </br>' . $row['message'] . " " . $row['category'] . "<br />" ;
+    echo $row['id'] . " " . $row['username'] . " " . $row['title'] . '<img src="../includes/' . $row['image'] . '"/> </br>' . $row['message'] . " " . $row['category'] . "<br />" ;
     echo "</pre>";
 // echo "<form method='POST' action='viewPosts.php'>
 //       <input type='submit' name='commentBtn' value='comment!'>
