25aaeb911ec0a087f17712a550a2f4070b0ee616
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-02-28 18:25:54 +0100

    fixed size to img

--- views/viewPosts.php
+++ views/viewPosts.php
@@ -30,7 +30,7 @@ while ($row = $stm->fetch()) {
 
 
     }
-    echo $row['id'] . " " . $row['username'] . " " . $row['title'] . '<img src="../includes/' . $row['image'] . '"/> </br>' . $row['message'] . " " . $row['category'] . "<br />" ;
+    echo $row['id'] . " " . $row['username'] . " " . $row['title'] . '<img src="../includes/' . $row['image'] . '" "height=200 width=300"/> </br>' . $row['message'] . " " . $row['category'] . "<br />" ;
     echo "</pre>";
 // echo "<form method='POST' action='viewPosts.php'>
 //       <input type='submit' name='commentBtn' value='comment!'>
